8f584d158de2d6a32c8c3548814319145d75932d
enyen <puangenyen@gmail.com>
2023-08-23 04:50:29 +0800

    xml update

--- envs/assets/unstable_grasp/inspect_env.py
+++ envs/assets/unstable_grasp/inspect_env.py
@@ -1,13 +1,26 @@
 import redmax_py as redmax
 import numpy as np
 
-# sim = redmax.Simulation('./unstable_grasp.xml', verbose=True)
-sim = redmax.Simulation('../stable_grasp/stable_grasp.xml', verbose=True)
-sim.viewer_options.camera_lookat = np.array([0., 0., 1])
-sim.viewer_options.camera_pos = np.array([2., -2., 1.])
+sim = redmax.Simulation('./unstable_grasp.xml', verbose=True)
 sim.viewer_options.loop = False
 sim.viewer_options.infinite = False
-sim.viewer_options.speed = 0.4
+sim.viewer_options.speed = 1
+
+qpos_init = sim.get_q_init().copy()
+print("q_init: ", qpos_init.shape)
+qpos_init[1] = -0.05
+qpos_init[2] = 0.166
+qpos_init[4] = -0.02
+qpos_init[5] = -0.02
+qpos_end = qpos_init.copy()
+qpos_end[2] = 0.21
+sim.set_q_init(qpos_init)
 sim.reset(backward_flag=False)
-sim.forward(100)
+for t in range(500):
+    u = qpos_init[:6] + t / 500. * (qpos_end[:6] - qpos_init[:6])
+    # u[4] = 0.04
+    # u[5] = 0.04
+    sim.set_u(u)
+    sim.forward(1, verbose=False, test_derivatives=False)
+# sim.forward(1000, verbose=False, test_derivatives=False)
 sim.replay()
